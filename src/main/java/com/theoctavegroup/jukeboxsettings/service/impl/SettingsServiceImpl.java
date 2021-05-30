b8d754d622dc978043ac3a2de1df4d2aafcaf1fc
imad <imad.salki@hotmail.fr>
2021-05-31 00:15:04 +0200

    Add Settings cache clean

--- src/main/java/com/theoctavegroup/jukeboxsettings/service/impl/SettingsServiceImpl.java
+++ src/main/java/com/theoctavegroup/jukeboxsettings/service/impl/SettingsServiceImpl.java
@@ -5,6 +5,7 @@ import com.theoctavegroup.jukeboxsettings.dto.SettingPropertiesDTO;
 import com.theoctavegroup.jukeboxsettings.exception.ResourceNotFoundException;
 import com.theoctavegroup.jukeboxsettings.service.SettingsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
@@ -21,8 +22,11 @@ public class SettingsServiceImpl implements SettingsService {
     // Settings API
     SettingsApi settingsApi;
 
+    // Cache duration
+    public static final int CACHE_DURATION_MINUTE = 1;
+
     // Settings Concurrent HashMap
-    Map<String, SettingPropertiesDTO> conSettingsMap = new ConcurrentHashMap<>();
+    Map<String, SettingPropertiesDTO> settingsCache = new ConcurrentHashMap<>();
 
     @Autowired
     SettingsServiceImpl(SettingsApi settingsApi) {
@@ -35,23 +39,31 @@ public class SettingsServiceImpl implements SettingsService {
     @Override
     public SettingPropertiesDTO getSettingById(String id) {
 
-        SettingPropertiesDTO settingFound = conSettingsMap.get(id);
+        SettingPropertiesDTO settingFound = settingsCache.get(id);
 
         // If the ConcurrentHashMap Doesn't have the settings we are searching, we reload the Data
         if (settingFound == null) {
 
             // Reloading all the Settings
-            conSettingsMap = this.settingsApi.getAllSettings().stream()
+            settingsCache = this.settingsApi.getAllSettings().stream()
                     .collect(Collectors.toMap(SettingPropertiesDTO::getId, Function.identity()));
 
             // Get Settings by ID
-            settingFound = conSettingsMap.get(id);
+            settingFound = settingsCache.get(id);
 
             // If we still don't find the Setting we throw a Resource Not Found Exception
             if (settingFound == null) throw new ResourceNotFoundException("Setting Not Found");
         }
 
         return settingFound;
+    }
+
+    /**
+     * Clean cache after $CACHE_DURATION_MINUTE minutes
+     */
+    @Scheduled(fixedDelay = CACHE_DURATION_MINUTE * 60_000)
+    private void cleanSettingsCache() {
+        settingsCache.clear();
     }
 
 }
